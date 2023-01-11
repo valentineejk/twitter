8e7da4dfa18a2279a880ee2909cd73ca2927087b
valentineejk <valentineejk@gmail.com>
2023-01-11 21:06:33 +0100

    Initial Commit

---
+++ twitter/twitterApp.swift
@@ -0,0 +1,17 @@
+//
+//  twitterApp.swift
+//  twitter
+//
+//  Created by Valentineejk on 11/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct twitterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
